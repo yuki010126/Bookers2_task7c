4587402008d98ec902dca8d6cded7343882f496d
yuki010126 <kinyounokyuzitu@gmail.com>
2021-08-04 07:40:24 +0000

    task4_end

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,17 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
- 
+
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
-    resource :favorites, only: [:create, :destroy]
-    resources :comments, only: [:create, :destroy]
+  resource :favorites, only: [:create, :destroy]
+  resources :comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :edit, :update]
+
+  resources :users, only: [:index, :show, :edit, :update] do
+  resource :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_125054) do
+ActiveRecord::Schema.define(version: 2021_08_03_055747) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -35,13 +35,11 @@ ActiveRecord::Schema.define(version: 2021_07_30_125054) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer "book_id"
-    t.integer "user_id"
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["book_id"], name: "index_likes_on_book_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
